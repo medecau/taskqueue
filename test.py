5887303086de5f21411762f6fc01170a7086e35e
Pedro Rodrigues <medecau@gmail.com>
2011-05-27 01:42:18 +0100

    a more cpu bound test

--- test.py
+++ test.py
@@ -1,32 +1,46 @@
 from taskqueue import Queue
 from taskqueue import Task
-from time import sleep
+from time import time, sleep
+
+def euler5(n):
+    init=time()
+    divs=range(n,0,-1)
+    for e in divs:
+        for f in divs[::-1]:
+            if e%f==0 and e!=f:
+                divs.remove(f)
+
+    num=0
+    not_found=True
+    while not_found:
+        num+=n
+        not_found=False
+        for each in divs:
+            if num%each!=0:
+                not_found=True
+                break
+
+    return (time()-init, num)
 
 def main():
     # ---------
     # Example
     # ---------
     from random import random
-    
-    def writethis(msg, msgr='anon'):
-        sleep(random()*0.5)
-        return '%s: %s' % (msgr, msg)
-
+    init=time()
     q=Queue()
-    for i in range(10):
-        q.append(Task(writethis, (i, 'desu')))
-        q.append(Task(writethis, (i,)))
+    for i in range(6):
+        q.append(Task(euler5, (17,)))
+        q.append(Task(euler5, (11,)))
 
     print 'waiting...'
 
     q.wait()
     print 'printing...'
     while not q.is_empty():
-        print '.',
-        print q.pop().result
-    print q.is_empty()
+        print 'took: %f - value: %d' % q.pop().result
     q.die()
-
+    print 'total running time: %f' % (time()-init)
 
 
 if __name__ == '__main__':
